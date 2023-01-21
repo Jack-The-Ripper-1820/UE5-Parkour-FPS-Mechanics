91f024a80e9f92fa9a9e50356e6d2862ff583c4e
Mayur <mayurjreddy@gmail.com>
2023-01-20 22:37:43 -0500

    AI Fire Function

---
+++ Source/ArenaShooter/ArenaShooter.Build.cs
@@ -0,0 +1,24 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+using UnrealBuildTool;
+
+public class ArenaShooter : ModuleRules
+{
+	public ArenaShooter(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+	
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "GameplayTasks"});
+
+		PrivateDependencyModuleNames.AddRange(new string[] { "OnlineSubsystem" , "OnlineSubsystemSteam"});
+
+		// Uncomment if you are using Slate UI
+		PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore",  });
+		
+		// Uncomment if you are using online features
+		//PrivateDependencyModuleNames.Add("OnlineSubsystem");
+		//PrivateDependencyModuleNames.Add("OnlineSubsystemSteam");
+
+		// To include OnlineSubsystemSteam, add it to the plugins section in your uproject file with the Enabled attribute set to true
+	}
+}
---
+++ Source/ArenaShooter/BTTask_ClearBlackboardValue.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "D:\Unreal Engine\UE_5.1\Engine\Source\Runtime\AIModule\Classes\BehaviorTree\Tasks\BTTask_BlackboardBase.h"
+#include "BTTask_ClearBlackboardValue.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ARENASHOOTER_API UBTTask_ClearBlackboardValue : public UBTTask_BlackboardBase
+{
+	GENERATED_BODY()
+	
+};
