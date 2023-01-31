209e8a2856a16cad8e6679476ebc5caec3cb62b2
Mayur <mayurjreddy@gmail.com>
2023-01-31 11:18:05 -0500

    Fixed Build Errors

--- Source/ArenaShooter/ArenaShooter.Build.cs
+++ Source/ArenaShooter/ArenaShooter.Build.cs
@@ -8,9 +8,9 @@ public class ArenaShooter : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "GameplayTasks"});
-
-		PrivateDependencyModuleNames.AddRange(new string[] { "OnlineSubsystem" , "OnlineSubsystemSteam"});
+		//PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "GameplayTasks"});
+        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "AIModule", "GameplayTasks" });
+        PrivateDependencyModuleNames.AddRange(new string[] { "OnlineSubsystem" , "OnlineSubsystemSteam"});
 
 		// Uncomment if you are using Slate UI
 		PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore",  });
